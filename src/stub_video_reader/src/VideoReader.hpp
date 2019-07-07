bd60c7be29108d176e462fa684d4937df774317d
jakub-tomczak <jakub.tomczak@live.com>
2019-07-07 18:51:51 +0200

    Add stub_video_reader.

---
+++ src/stub_video_reader/src/VideoReader.hpp
@@ -0,0 +1,8 @@
+#include <ros/ros.h>
+
+class VideoReader
+{
+    public:
+        VideoReader() {}
+        ~VideoReader() {}
+};
---
+++ src/stub_video_reader/src/main.cpp
@@ -0,0 +1,10 @@
+#include "VideoReader.hpp"
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
