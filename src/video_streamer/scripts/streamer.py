13c1fdfcf50124d248800fea8f7221abbe558335
jakub-tomczak <jakub.tomczak@live.com>
2019-07-07 22:18:47 +0200

    Convert image to appropriate format.

--- src/video_streamer/scripts/streamer.py
+++ src/video_streamer/scripts/streamer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 import cv2
+import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from camera_streamer import CameraStreamer
@@ -19,6 +20,7 @@ def main():
 	rospy.init_node(node_name, anonymous=True)
 
 	cv_bridge = CvBridge()
+
 	publisher = rospy.Publisher(VideoStreamer.topic_name, Image, queue_size=10)
 
 	video_streamer = CameraStreamer(target_fps, target_width, target_height)
@@ -30,7 +32,7 @@ def main():
 		try:
 			ret, frame = video_streamer.get_stream()
 			if ret:
-				message = cv_bridge.cv2_to_imgmsg(frame, encoding='passthrough')
+				message = cv_bridge.cv2_to_imgmsg(frame, encoding='bgr8')
 				publisher.publish(message)
 			else:
 				rospy.logwarn('Failed to capture a frame.')
