18bb763632911e4bdee9ff0960216e89d653a266
jakub-tomczak <jakub.tomczak@live.com>
2019-10-02 23:43:38 +0200

    Run another instance of the existing ghost-racer container.

--- docker_run.bash
+++ docker_run.bash
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+RACER_DOCKER_CONTAINER_ID=$(sudo docker ps -aqf "name=ghost-racer")
+
+if [ ! -z "$RACER_DOCKER_CONTAINER_ID" ];then
+    # exec existing container
+    echo "Opening existing ghost-racer's container [$RACER_DOCKER_CONTAINER_ID]"
+    sudo docker exec -it $RACER_DOCKER_CONTAINER_ID /bin/bash
+    exit 0
+fi
+
 ghost_racer_root_dir=$1
 docker_ghost_racer_root_dir=/home/ghost/ghost-racer
 
@@ -21,4 +30,5 @@ docker run -it --rm \
     -p 11311:11311 \
     -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
     -v $ghost_racer_root_dir:$docker_ghost_racer_root_dir \
+    --name "ghost-racer" \
     jakubtomczak/ghost-racer
